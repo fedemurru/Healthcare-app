5331c71d19a66c267404be72c3878a4c9f555f2f
Federico.Murru <djfedemurru@icloud.com>
2024-08-28 13:45:28 +0100

    form update

--- components/forms/PatientForms.tsx
+++ components/forms/PatientForms.tsx
@@ -4,6 +4,18 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 const formSchema = z.object({
 	username: z.string().min(2, {
 		message: "Username must be at least 2 characters.",
@@ -24,7 +36,29 @@ export function PatientForms() {
 		// Do something with the form values.
 		console.log(values);
 	}
-	return <div>Patient Forms</div>;
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="username"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Username</FormLabel>
+							<FormControl>
+								<Input placeholder="Enter username" {...field} />
+							</FormControl>
+							<FormDescription>
+								This is your public display name.
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
+	);
 }
 
 export default PatientForms;
