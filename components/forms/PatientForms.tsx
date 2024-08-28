c6f9c43a55e2831c27d7b7f9f49abdb7175bcfbc
Federico.Murru <djfedemurru@icloud.com>
2024-08-28 13:40:29 +0100

    form

--- components/forms/PatientForms.tsx
+++ components/forms/PatientForms.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
 
-const PatientForms = () => {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+	username: z.string().min(2, {
+		message: "Username must be at least 2 characters.",
+	}),
+});
+
+export function PatientForms() {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: "",
+		},
+	});
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		console.log(values);
+	}
 	return <div>Patient Forms</div>;
-};
+}
 
 export default PatientForms;
