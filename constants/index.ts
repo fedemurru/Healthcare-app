ba0a9dfddf6bd2cd96314c48bac408086234ce20
Federico.Murru <djfedemurru@icloud.com>
2024-09-05 07:39:50 +0100

    canceled

--- constants/index.ts
+++ constants/index.ts
@@ -82,5 +82,5 @@ export const Doctors = [
 export const StatusIcon = {
 	scheduled: "/assets/icons/check.svg",
 	pending: "/assets/icons/pending.svg",
-	cancelled: "/assets/icons/cancelled.svg",
+	canceled: "/assets/icons/cancelled.svg",
 };
